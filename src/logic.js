99e7a976de5826c875476087b2808447493e9c25
nytorq <charlieprator@gmail.com>
2024-09-06 12:52:39 -0600

    Cleaned up logic.js and added conditional to create localStorage.appData data when not present

--- src/logic.js
+++ src/logic.js
@@ -1,12 +1,3 @@
-import {idCreator} from "./tools.js";
-
-// Classes
-// class User {
-//     projects = [];
-//     addProject(project) {
-//         this.projects.push(project);
-//     }
-// }
 class Project {
     constructor(name) {
         this.name = name;
@@ -34,15 +25,6 @@ class Task {
 
 const storageKey = 'appData';
 
-/*
- - Create new X
- - Retrieve appData. Convert from string to object
- - Insert X into its respective place
- - Stringify object
- - Save into localStorage
-*/
-
-
 const addProject = function(projectName) {
     let newProject = new Project(projectName);
     let parsedAppData = loadFromLocalStorage();
@@ -58,14 +40,9 @@ const addTask = function(project, title, description, date, priority) {
     let taskObject = new Task(title, description, date, priority);
     let parsedAppData = loadFromLocalStorage();
     let matchedProject = parsedAppData.projects.filter((obj) => obj.name === project);
-    // appData.projects[0].tasks.push(taskObject);
-    // console.log('These are the matching projects: ', matchedProject[0])
-    // console.log('The matchedProject has an addTask method:', typeof matchedProject[0].addTask === 'function');
-    // console.log('This is parsedAppData before rewrite: ', parsedAppData)
     matchedProject[0].tasks.push(taskObject);
-    // console.log('This is parsedAppData AFTER rewrite: ', parsedAppData)
     saveToLocalStorage(parsedAppData);
-    console.log(`%cA new task has been added to ${matchedProject[0].name}:`, 'color: blue;');
+    console.log(`%cA new task has been added to the project "${matchedProject[0].name}":`, 'color: blue;');
     return taskObject;
 }
 
@@ -86,31 +63,6 @@ const editTask = function(project, taskID, taskProperty, value) {
     saveToLocalStorage(parsedAppData);
     console.log(`%c${project}'s task #${taskID} property, "${taskProperty}" has been changed to: `, 'color: blue;', targetTask[0]);
 }
-
-// const updateLocalStorage = function(object) {
-//     const JSONString = JSON.stringify(object);
-//     localStorage.setItem('currentUser', JSONString);
-// }
-
-// // Creating basic data structure
-
-
-// const updateAppData = function() {
-//     if (localStorage.currentUser) {
-//         let JSONString = localStorage.getItem('currentUser');
-//         currentUser = JSON.parse(JSONString);
-//     } else {
-//         currentUser = new User();
-//         project1 = new Project();
-//         currentUser.addProject(project1);
-//         updateLocalStorage(currentUser)
-//     }
-// }
-
-// updateAppData();
-
-// Data creation functions
-
 
 const saveToLocalStorage = function(object) {
     try {
@@ -147,20 +99,9 @@ window.editTask = editTask;
 window.addProject = addProject;
 window.createAppData = createAppData;
 
-/* 
-1. See if there's appData to pull from
-    1. If there isn't, go ahead and create a new User and a Project. Then run saveToLocalStorage.
-    2. If there is, loadFromLocalStorage. Then populate you app with appData
-
-2. Adding a task
-    1. Perform loadFromLocalStorage
-    2. Navigate down to curren project
-    3. Add new task to tasks array
-    4. Run STLS
-*/ 
-
-const testProject = new Project('testProject');
-testProject.addTask('testTask');
-window.testProject = testProject;
+if (!localStorage.appData) {
+    createAppData();
+    addProject('Personal');
+}
 
 export {Project, Task, addTask, loadFromLocalStorage}
